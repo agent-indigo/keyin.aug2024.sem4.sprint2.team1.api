86ce4509b46be26f0f10e2ad654d4b133e0083ac
agent-indigo <bradenhynes@gmail.com>
2024-07-23 13:17:09 -0230

    Bug fix

--- src/main/java/com/keyin/aug2024/sem4/sprint2/team1/api/interfaces/Serve.java
+++ src/main/java/com/keyin/aug2024/sem4/sprint2/team1/api/interfaces/Serve.java
@@ -2,5 +2,5 @@ package com.keyin.aug2024.sem4.sprint2.team1.api.interfaces;
 import org.springframework.stereotype.Service;
 @Service
 public interface Serve {
-    void delete(int pk);
+    String delete(int pk);
 }
