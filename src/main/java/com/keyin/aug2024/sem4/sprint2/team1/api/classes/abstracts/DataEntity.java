36c90e04354fb77cea97627d46743801df2c80d5
agent-indigo <bradenhynes@gmail.com>
2024-07-28 19:18:55 -0230

    Bug fixes

--- src/main/java/com/keyin/aug2024/sem4/sprint2/team1/api/classes/abstracts/DataEntity.java
+++ src/main/java/com/keyin/aug2024/sem4/sprint2/team1/api/classes/abstracts/DataEntity.java
@@ -1,12 +1,15 @@
 package com.keyin.aug2024.sem4.sprint2.team1.api.classes.abstracts;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import java.util.UUID;
+@MappedSuperclass
 public abstract class DataEntity {
     @Id
-    @GeneratedValue(generator = "UUID")
-    protected UUID pk;
-    protected boolean active;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID pk;
+    private boolean active;
     public DataEntity() {
         this.pk = UUID.randomUUID();
         this.active = true;
